517ce4d507f85f87d4072a59593211bfc107ae69
stepanenkokir <stepanenkokir@gmail.com>
2024-08-18 18:31:18 -0700

    add config

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import config from 'config'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
-const PORT = 50006
+const PORT =  config.has('port')? config.get('port'): 50007
 
 app.use(express.static(path.join(__dirname, 'client', 'dist')))
 
